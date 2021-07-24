e158e4add3adb42458500b0d3c0be3088a4a2cc9
hyoukim <hyoukim@hyoukimui-MacBookPro.local>
2021-07-24 18:14:33 +0900

    particle done

---
+++ opengl_game_prac/ParticleGenerator.cpp
@@ -0,0 +1,8 @@
+//
+//  ParticleGenerator.cpp
+//  opengl_game_prac
+//
+//  Created by hyoukim on 2021/07/24.
+//
+
+#include "ParticleGenerator.hpp"
---
+++ opengl_game_prac/ParticleGenerator.hpp
@@ -0,0 +1,13 @@
+//
+//  ParticleGenerator.hpp
+//  opengl_game_prac
+//
+//  Created by hyoukim on 2021/07/24.
+//
+
+#ifndef ParticleGenerator_hpp
+#define ParticleGenerator_hpp
+
+#include <stdio.h>
+
+#endif /* ParticleGenerator_hpp */
